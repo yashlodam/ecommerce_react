bffbbb92f07883ebe1b96f421264c265ad78462f
Yash Lodam <yashlodam03@gmail.com>
2026-06-22 17:25:07 +0530

    added drawer

--- ecommerce_react/src/seller/components/DrawerList.jsx
+++ ecommerce_react/src/seller/components/DrawerList.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import AddIcon from '@mui/icons-material/Add';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LogoutIcon from '@mui/icons-material/Logout';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
-function DrawerList() {
+function DrawerList({menu,menu2,toggleDrawer}) {
+
+    
   return (
-    <div>DrawerList</div>
+    <div className='h-full'>
+        <div className='flex flex-col justify-between h-full w-[300px] border-r py-5'>
+
+            <div>
+                <div className='space-y-2'>
+
+         {
+            menu.map((item,index)=>{
+                <div key={index}>
+                    <p>
+                       <ListItemIcon>
+                        {item.icon}
+                        </ListItemIcon> 
+                        ListItemIcon
+                    </p>
+                </div>
+            })
+         }
+
+                </div>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
--- ecommerce_react/src/seller/components/SellerDrawerList.jsx
+++ ecommerce_react/src/seller/components/SellerDrawerList.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import DrawerList from './DrawerList'
 
 function SellerDrawerList() {
+    const menu = [
+            {
+                name:"Dashboard",
+                path:"/seller",
+                icon:<Dashboard className="text-primary"/>,
+                activeIcon:<Dashboard className="text-white"/>,
+            },
+            {
+                name:"Seller",
+                path:"/seller/orders",
+                icon:<ShoppingBagIcon className="text-primary"/>,
+                activeIcon:<ShoppingBagIcon className="text-white"/>,
+            },
+            {
+                name:"Orders",
+                path:"/seller/orders",
+                icon:<ShoppingBagIcon className="text-primary"/>,
+                activeIcon:<ShoppingBagIcon className="text-white"/>,
+            },
+            {
+                name:"Products",
+                path:"/seller/products",
+                icon:<InventoryIcon className="text-primary"/>,
+                activeIcon:<InventoryIcon className="text-white"/>,
+            },
+            {
+                name:"Add Product",
+                path:"/seller/add-products",
+                icon:<AddIcon className="text-primary"/>,
+                activeIcon:<AddIcon className="text-white"/>,
+            },
+            {
+                name:"Payment",
+                path:"/seller/payment",
+                icon:<AccountBalanceWalletIcon className="text-primary"/>,
+                activeIcon:<AccountBalanceWalletIcon  className="text-white"/>,
+            },
+            {
+                name:"Transaction",
+                path:"/seller/transaction",
+                icon:<ReceiptIcon className="text-primary"/>,
+                activeIcon:<ReceiptIcon className="text-white"/>,
+            },
+            
+    
+        ]
+    
+        const menu2 = [
+            {
+                name:"Account",
+                path:"/seller/account",
+                icon:<AccountBoxIcon className="text-primary"/>,
+                activeIcon:<AccountBoxIcon className="text-white"/>,
+            },
+            {
+                name:"Logout",
+                path:"/",
+                icon:<LogoutIcon className="text-primary"/>,
+                activeIcon:<LogoutIcon className="text-white"/>,
+            },
+        ]
   return (
     <div>
-        
+        <DrawerList menu={menu} menu2={menu2} toggleDrawer={true}/>
     </div>
   )
 }
