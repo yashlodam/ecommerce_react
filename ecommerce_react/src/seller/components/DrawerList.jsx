1492e772f98c8e6da75746517838cb37ea42a45d
Yash Lodam <yashlodam03@gmail.com>
2026-06-22 13:51:50 +0530

    implementing drawer section for  seller

---
+++ ecommerce_react/src/seller/components/DrawerList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DrawerList() {
+  return (
+    <div>DrawerList</div>
+  )
+}
+
+export default DrawerList
--- ecommerce_react/src/seller/pages/SellerDashboard/SellerDashboard.jsx
+++ ecommerce_react/src/seller/pages/SellerDashboard/SellerDashboard.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function SellerDashboard() {
   return (
     <div>
-        
+
+        <div className='lg:flex lg:h-[90vh]'>
+            <section className='hidden lg:block h-full'>
+                SellerDrawerList
+            </section>
+            <section className='p-10 w-full lg:w-[80%] overflow-y-auto'>
+                seller routes
+            </section>
+        </div>
+
     </div>
   )
 }
