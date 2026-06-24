5b05c27fc067a77ae705cc61746e9db99ca740ea
Yash Lodam <yashlodam03@gmail.com>
2026-06-24 21:48:52 +0530

    added store.js for redux toolkit

--- ecommerce_react/src/State/Store.js
+++ ecommerce_react/src/State/Store.js
@@ -1,12 +1,17 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import todosReducer from '../features/todos/todosSlice'
-import filtersReducer from '../features/filters/filtersSlice'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+
+import todosReducer from "../features/todos/todosSlice";
+import filtersReducer from "../features/filters/filtersSlice";
 
 const rootReducer = combineReducers({
-    
-})
+  todos: todosReducer,
+  filters: filtersReducer,
+});
 
-export const Store = configureStore({
-  reducer:rootReducer
-})
+export const store = configureStore({
+  reducer: rootReducer,
+});
 
+export const useAppDispatch = () => useDispatch();
+export const useAppSelector = useSelector;
