9f6762b75a6e714c3deba4ffe20f79ce0a88689c
Yash Lodam <yashlodam03@gmail.com>
2026-06-27 14:18:59 +0530

    integrating apis for add to cart

--- ecommerce_react/src/State/customer/CartSlice.js
+++ ecommerce_react/src/State/customer/CartSlice.js
@@ -38,3 +38,96 @@ export const addItemToCart = createAsyncThunk("/cart/addItemToCart",
         }
     }
 )
+
+
+export const deleteCartItem = createAsyncThunk("/cartdeleteCartItem",
+    async(jwt,cartItemId,{rejectWithValue})=>{
+        try{
+            const response = await api.delete(`${API_URL}/item/${cartItemId}`,{
+                headers:{
+                    Authorization: `Bearer ${jwt}`
+                }
+            })
+
+            return response.data
+        }
+        catch(error){
+            console.log(error);
+            return rejectWithValue(
+                error.response.data.message || "Failed to delete cart item"
+            )
+        }
+    }
+)
+
+export const updateCartItem = createAsyncThunk("cart/updateCartItem",
+    async(jwt,cartItemId,cartItem,{rejectWithValue})=>{
+        try{
+            const response = await api.put(`${API_URL}/item/${cartItemId}`,
+                cartItem,
+                {
+                    headers : {
+                        Authorization : `Bearer ${jwt}`
+                    }
+                }
+            )
+
+            return response.data
+        }
+        catch(errror){
+            return rejectWithValue(
+                error.response.data.message || "Failed to update cart item"
+            )
+        }
+    }
+)
+
+const initialState = {
+    cart : null,
+    loading : false,
+    error: null,
+
+};
+
+
+const cartSlice = createSlice({
+    name : "cart",
+    initialState,
+    reducers:{
+        resetCartState : (state)=>{
+            state.cart = null;
+            state.loading = false;
+            state.error = null;
+        }
+    },
+    extraReducers: (builder)=>{
+        builder.addCase(fetchUserCart.pending,(state)=>{
+            state.loading = true;
+            state.error = null;
+        })
+        builder.addCase(fetchUserCart.fulfilled,(state,action)=>{
+            state.cart = action.payload;
+            state.loading = false
+        })
+
+        builder.addCase(fetchUserCart.rejected,(state,action)=>{
+            state.loading=false;
+            state.error = action.payload
+        })
+        builder.addCase(addItemToCart.pending,(state)=>{
+            state.loading = true;
+            state.error = null;
+        })
+        builder.addCase(addItemToCart.fulfilled,(state,action)=>{
+            if(state.cart){
+               state.cart.cartItems.push(action.payload);
+            }
+            state.loading = false;
+        })
+
+        builder.addCase(addItemToCart.rejected, (state,action)=>{
+            state.loading = false;
+            state.error = action.payload
+        })
+    }
+})
