244a2e306f8b9748fdaf509929d56ff47a3e52dc
Yash Lodam <yashlodam03@gmail.com>
2026-06-30 16:11:02 +0530

    transactionSlice are created

--- ecommerce_react/src/State/seller/transactionSlice.js
+++ ecommerce_react/src/State/seller/transactionSlice.js
@@ -1,32 +1,70 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { api } from "../../config/Api";
 
 const initialState = {
-    transactions : [],
-    transaction : null,
-    loading : false,
-    error:null
-}
+  transactions: [],
+  transaction: null,
+  loading: false,
+  error: null,
+};
 
+export const fetchTransactionsBySeller = createAsyncThunk(
+  "transactions/fetchTransactionsBySeller",
+  async (jwt, { rejectWithValue }) => {
+    try {
+      const response = await api.get("/api/transactions/seller", {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
 
-export const fetchTransactionsBySeller = createAsyncThunk("transactions/fetchTransactionsBySeller",
-    async(jwt,{rejectWithValue})=>{
-        try{
-            const response = await api.get("/api/transactions/seller",{
-                headers:{
-                    Authorization: `Bearer ${jwt}`
-                }
-            })
+      console.log("fetchTransactionsBySeller", response.data);
 
-            console.log("fetchtransactionsByseller",response.data);
+      return response.data;
+    } catch (error) {
+      if (error.response) {
+        return rejectWithValue(
+          error.response.data.message || "Failed to fetch transactions"
+        );
+      }
 
-            return response.data;
-        }
-        catch(error){
-            if(error.response){
-                return rejectWithValue(error.response.data.message);
-            }
-            return rejectWithValue("Failed to fetch transactions");
-        }
+      return rejectWithValue("Failed to fetch transactions");
     }
-)
+  }
+);
+
+const transactionSlice = createSlice({
+  name: "transactions",
+  initialState,
+  reducers: {
+    resetTransactionState: (state) => {
+      state.transactions = [];
+      state.transaction = null;
+      state.loading = false;
+      state.error = null;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      // Fetch Transactions
+      .addCase(fetchTransactionsBySeller.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+
+      .addCase(fetchTransactionsBySeller.fulfilled, (state, action) => {
+        state.loading = false;
+        state.transactions = action.payload;
+      })
+
+      .addCase(fetchTransactionsBySeller.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { resetTransactionState } = transactionSlice.actions;
+
+export default transactionSlice.reducer;
